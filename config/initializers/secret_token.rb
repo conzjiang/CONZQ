4b39e7863e2b0c2e8c92c121b90205397c7f9c21
Constance Jiang <conzjiang@gmail.com>
2014-06-24 10:14:56 -0400

    Initial commit: create pg database, update gemfile, remove turbolinks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CONZQ::Application.config.secret_key_base = '9d766030fcadbd679bfad1e00e6fb1386ce5da1ba3d7125e2803e743102a93357a3508a5d98a8d274d82f46f5736a69156ff60ec617165a63f95897cb1b6a8b2'
