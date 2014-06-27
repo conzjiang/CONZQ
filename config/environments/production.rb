e998361daf35658d0b9d1095db816cfc1beb3a53
App Academy Student <student@appacademy.io>
2014-06-27 16:00:52 -0400

    Take more stuff out for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ CONZQ::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,17 +77,17 @@ CONZQ::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
-  
-  config.paperclip_defaults = {                                   
-    :storage => :s3,                                              
-    :s3_protocol => 'http',                                       
-    :url =>':s3_domain_url',                                      
+
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :url =>':s3_domain_url',
     :path => '/:class/:attachment/:id_partition/:style/:filename',
-    :s3_credentials => {                                          
-      :bucket => ENV['S3_PROD_BUCKET'],               
-      :access_key_id => ENV['S3_ACCESS_KEY'],                 
-      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']          
-    }                                                             
-  } 
+    :s3_credentials => {
+      :bucket => ENV['S3_PROD_BUCKET'],
+      :access_key_id => ENV['S3_ACCESS_KEY'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+    }
+  }
 end
