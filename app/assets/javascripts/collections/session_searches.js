9899338bdf4b67f69dee98373729ee746fd2035e
Constance Jiang <conzjiang@gmail.com>
2014-07-03 09:25:39 -0400

    Fix comparison error in SessionSearches#prevSearch

--- app/assets/javascripts/collections/session_searches.js
+++ app/assets/javascripts/collections/session_searches.js
@@ -5,12 +5,14 @@ CONZQ.Collections.SessionSearches = Backbone.Collection.extend({
 	
 	prevSearch: function (params) {
 		var searchParams = params.search;
-		debugger
+		
 		return this.find(function (prevQuery) {
 			var prevSearchParams = prevQuery.get("search");
 			
-			var decadeIds = prevSearchParams.decade_ids === searchParams.decade_ids;
-			var genreIds = prevSearchParams.genre_ids === searchParams.genre_ids;
+			var decadeIds = _.isEqual(prevSearchParams.decade_ids,
+																searchParams.decade_ids);
+			var genreIds = _.isEqual(prevSearchParams.genre_ids,
+															 searchParams.genre_ids);
 			var stat = prevSearchParams.status === searchParams.status;
 			
 			return decadeIds && genreIds && stat;
